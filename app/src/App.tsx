26ae7efc8ce6e4b5fb5d51c0110749138cf6d047
melany estevez camata <elibialany@gmail.com>
2023-02-25 11:50:08 -0400

    reducer

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
-import './style/styles.css'
-import InputField from './components/InputField';
-import TodoList from './components/TodoList';
-import { Todo } from './components/model';
+import './style/styles.css';
 function App() {
-  const [todos, setTodos] = useState<Array<Todo>>([]);
-  const [todo, setTodo] = useState<string>("")
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
-      setTodo("")
-    }
-  }
+
   return (
-    <div className="App">
-      <span className='heading'>
-        Tasking
-      </span>
-      <div className='input'>
-        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      </div>
-        <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+    <p>hello</p>
   );
 }
 
